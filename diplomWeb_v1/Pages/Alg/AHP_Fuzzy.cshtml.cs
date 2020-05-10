9087a840456d69056e3eff2fb14dcb6569220fc9
Toga <rodionphilippov@gmail.com>
2020-05-11 01:28:53 +0400

    created

---
+++ diplomWeb_v1/Pages/Alg/AHP_Fuzzy.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
+
+namespace diplomWeb_v1.Pages.Alg
+{
+    public class AHP_FuzzyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public List<Criteria> Criterias { get; set; }
+        public string Message { get; private set; }
+
+        public AHP_FuzzyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+            Criterias = new List<Criteria>();
+        }
+
+        public void OnGet()
+        {
+            
+        }
+
+        public void OnPost(string CriteriaName, double CriteriaWeight)
+        {
+            Criterias.Add(new Criteria(CriteriaName, CriteriaWeight));
+        }
+    }
+}
